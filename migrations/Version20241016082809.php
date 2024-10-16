095f416ec29faf8ed7d2a838d04d8a419ebb233d
WebDevCF2m <gitweb@cf2m.be>
2024-10-16 10:28:34 +0200

    update User Entity

---
+++ migrations/Version20241016082809.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs! 😎
+ */
+final class Version20241016082809 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+    // YEEEAHHH 😍
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD user_full_name VARCHAR(200) DEFAULT NULL');
+    }
+    // NOOOO 😭
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP user_full_name');
+    }
+}
