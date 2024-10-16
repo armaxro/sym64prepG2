32fb998c9bdb207477bf05b2aa87bbaf2f5e7095
Maxi Rojas <arnaldomrojas@outlook.com>
2024-10-16 11:15:08 +0200

    fixtures

---
+++ migrations/Version20241016084949.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241016084949 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post ADD postslug VARCHAR(162) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD user_email VARCHAR(160) NOT NULL, ADD user_active TINYINT(1) DEFAULT 0 NOT NULL, ADD user_unique_key VARCHAR(255) NOT NULL, ADD user_full_name VARCHAR(200) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649550872C ON user (user_email)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post DROP postslug');
+        $this->addSql('DROP INDEX UNIQ_8D93D649550872C ON user');
+        $this->addSql('ALTER TABLE user DROP user_email, DROP user_active, DROP user_unique_key, DROP user_full_name');
+    }
+}
